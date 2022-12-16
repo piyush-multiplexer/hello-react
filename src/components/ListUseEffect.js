c3e94d99ba6ea88b49721c96c43e675a1702995f
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-12-16 11:06:52 +0530

    useTransition for performance - revisit

--- src/components/ListUseEffect.js
+++ src/components/ListUseEffect.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import NAMES from "./MOCK_DATA.json";
 
 export const ListUseEffect = () => {
   const [query, setQuery] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [isPending, startTransition] = useTransition();
+
   const changeHandler = (evt) => {
-    setQuery(evt.target.value);
+    setInputValue(evt.target.value);
+    startTransition(() => setQuery(evt.target.value));
   };
   const filteredNames = NAMES.filter((item) => {
     return item.first_name.includes(query) || item.last_name.includes(query);
   });
   return (
     <div>
-      <input type="text" value={query} onChange={changeHandler}></input>
+      <input type="text" value={inputValue} onChange={changeHandler}></input>
+      {isPending && <p>"Updating List.."</p>}
       {filteredNames.map((item) => (
         <p key={item.id}>
           {item.first_name} {item.last_name}
