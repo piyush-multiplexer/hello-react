949f226f016ab2e16cd36bf6318707aa0862ff66
Piyush Goyani <thesourcepedia@gmail.com>
2023-01-04 11:35:45 +0530

    redux: multiple/combined reducers

--- src/components/Redux/index.js
+++ src/components/Redux/index.js
@@ -1,12 +1,15 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICE = 'BUY_ICE'
 
 // State
-const initialState = {
+const initialCakeState = {
     numOfCakes: 8,
+}
+const initialIceState = {
     numOfIce: 15
 }
 
@@ -26,23 +29,35 @@ function buyIce() {
 }
 
 // Reducer (prev_state, action) => next_state
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
-        case BUY_ICE: return {
-            ...state,
-            numOfIce: state.numOfIce - 1
-        }
-
         default: return state
     }
 }
 
-const store = createStore(reducer)
-console.log('Initla State', store.getState())
+const iceReducer = (state = initialIceState, action) => {
+    switch (action.type) {
+        case BUY_ICE: return {
+            ...state,
+            numOfIce: state.numOfIce - 1
+        }
+        default: return state
+    }
+}
+
+// accepts only single reducer, so need to combine
+// it will update state key object and make it nested
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    ice: iceReducer
+})
+
+const store = createStore(rootReducer)
+console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyIce())
